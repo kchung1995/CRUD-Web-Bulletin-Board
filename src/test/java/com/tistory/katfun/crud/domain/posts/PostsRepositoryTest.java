2a65cff726b7259467570147a90a06807fed27e3
Kuyho Chung <kchung1995@gmail.com>
2022-02-16 21:15:53 +0900

    added PostsRepositoryTest.java

---
+++ src/test/java/com/tistory/katfun/crud/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.tistory.katfun.crud.domain.posts;
+
+//save, findall 기능을 테스트
+
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After(
+            public void cleanup() {
+                postsRepository.deleteAll();
+            }
+    )
+
+    @Test
+    public void 게시글저장_불러오기() {
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("kchung1995@gmail.com")
+                .build())
+        );
+    }
+
+    //when
+    List<Posts> postsList = postsRepository.findAll();
+
+    //then
+    Posts posts = postsList.get(0);
+    assertThat(posts.getTitle()).isEqualTo(title);
+    assertThat(posts.getContent()).isEqualTo(content);
+
+}
