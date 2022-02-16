40db6f2db1182505cd91d6cdc13bfee1015cdc1d
Kuyho Chung <kchung1995@gmail.com>
2022-02-16 23:33:41 +0900

    updated gradle to 7.0 and updated build.gradle

--- src/test/java/com/tistory/katfun/crud/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/tistory/katfun/crud/domain/posts/PostsRepositoryTest.java
@@ -2,45 +2,48 @@ package com.tistory.katfun.crud.domain.posts;
 
 //save, findall 기능을 테스트
 
-import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
-@RunWith(SpringRunner.class)
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class PostsRepositoryTest {
+
     @Autowired
     PostsRepository postsRepository;
 
-    @After(
-            public void cleanup() {
-                postsRepository.deleteAll();
-            }
-    )
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
 
     @Test
     public void 게시글저장_불러오기() {
-        String title = "테스트 게시글";
-        String content = "테스트 본문";
+        //given
+        String title = "test post";
+        String content = "test title";
 
         postsRepository.save(Posts.builder()
                 .title(title)
                 .content(content)
                 .author("kchung1995@gmail.com")
-                .build())
-        );
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertEquals(title, posts.getTitle());
+        assertEquals(content, posts.getContent());
     }
-
-    //when
-    List<Posts> postsList = postsRepository.findAll();
-
-    //then
-    Posts posts = postsList.get(0);
-    assertThat(posts.getTitle()).isEqualTo(title);
-    assertThat(posts.getContent()).isEqualTo(content);
-
 }
