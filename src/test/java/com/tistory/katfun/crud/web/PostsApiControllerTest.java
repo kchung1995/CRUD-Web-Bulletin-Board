508258919cf124affd4e9e319cefde42945b2b06
Kuyho Chung <kchung1995@gmail.com>
2022-02-21 22:14:00 +0900

    updated PostsApiControllerTest.java - test for update

--- src/test/java/com/tistory/katfun/crud/web/PostsApiControllerTest.java
+++ src/test/java/com/tistory/katfun/crud/web/PostsApiControllerTest.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -32,7 +34,7 @@ public class PostsApiControllerTest {
 
 
     @Test
-    public void Posts_Registered () throws Exception {
+    public void Posts_Registered() throws Exception {
         //given
         String title = "title";
         String content = "content";
@@ -56,5 +58,38 @@ public class PostsApiControllerTest {
         List<Posts> all = postsRepository.findAll();
         assertEquals(title, all.get(0).getTitle());
         assertEquals(content, all.get(0).getContent());
+    }
+
+    @Test
+    public void Posts_edited() throws Exception {
+        //given
+        Posts savedPosts = postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+        Long updateId = savedPosts.getId();
+        String expectedTitle = "title2";
+        String expectedContent = "content2";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(expectedTitle)
+                .content(expectedContent)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<PostsUpdateRequestDto>(requestDto);
+
+        //when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(
+                url, HttpMethod.PUT, requestEntity, Long.class);
+
+        //then
+        assertEquals(responseEntity.getStatusCode(), HttpStatus.OK);
+        Assertions.assertTrue(responseEntity.getBody() > 0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertEquals(all.get(0).getTitle(), expectedTitle);
+        assertEquals(all.get(0).getContent(), expectedContent);
     }
 }
