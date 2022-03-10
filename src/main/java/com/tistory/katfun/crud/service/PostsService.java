d8e3dba126de88eb86f67210f2b0d218750bd3d9
KuyHo <kchung1995@gmail.com>
2022-03-10 15:25:13 +0900

    added welcome screen and thymeleaf configurations

--- src/main/java/com/tistory/katfun/crud/service/PostsService.java
+++ src/main/java/com/tistory/katfun/crud/service/PostsService.java
@@ -31,12 +31,14 @@ public class PostsService {
 
     @Transactional
     public void delete (Long id) {
-        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다.\n게시글 ID: " + id));
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다.\n게시글 ID: " + id));
         postsRepository.delete(posts);
     }
 
     public PostsResponseDto findById (Long id) {
-        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다.\n게시글 ID: " + id));
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다.\n게시글 ID: " + id));
         return new PostsResponseDto(entity);
     }
 }
