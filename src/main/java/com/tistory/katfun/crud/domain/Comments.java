f181e06009268bc1695d8af16dff6dcf5d368382
KuyHo <kchung1995@gmail.com>
2022-07-22 22:57:28 +0900

    댓글 domain 추가

---
+++ src/main/java/com/tistory/katfun/crud/domain/Comments.java
@@ -0,0 +1,39 @@
+package com.tistory.katfun.crud.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Comments {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    @Column(nullable = false)
+    private String postId;
+
+    @Column(length = 100, nullable = false)
+    private String createId;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private Date lastEditTime;
+
+    @Type(type="yes_no")
+    @Column(length = 1)
+    private char secretYn;
+
+    @Type(type="yes_no")
+    @Column(length = 1)
+    private char editedYn;
+
+}
